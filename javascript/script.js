e13a4b8acf7b7570bd849a79e85da40650f67cb7
Joshua <hosanajoshua@gmail.com>
2020-05-18 17:37:48 +0300

    Added a rubric for the calculation of the pizza

--- javascript/script.js
+++ javascript/script.js
@@ -27,3 +27,21 @@ $("#checkout").click(function () {
         let order = (size, crust, topping, qunity, total) => {
             return {size, crust, topping, qunity, total};
         };
+        //check price
+        let price, totalPrice;
+        switch (size) {
+            case size = "small":
+                switch (size) {
+                    case size = "small":
+                        price = 150;
+                        if (crust === "thin") {
+                            totalPrice = (price * number) + 100;
+                        } else if (crust === "thick") {
+                            totalPrice = (price * number) + 150;
+                        } else if (crust === "flatbread") {
+                            totalPrice = (price * number) + 180;
+                        } else {
+                            totalPrice = (price * number) + 280;
+                        }
+                        break;
+                    
